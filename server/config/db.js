c59490749a76b1843f9d46381154ecec41da7ab6
MakWigglz <wigglz734@gmail.com>
2025-01-27 22:24:44 +0200

    Create db.js

---
+++ server/config/db.js
@@ -0,0 +1,23 @@
+/**
+ * Connects to the MongoDB database using the provided connection URI.
+ *
+ * @async
+ * @function connectDB
+ * @returns {Promise<void>} - Resolves when the connection is established, or rejects with an error.
+ */
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
